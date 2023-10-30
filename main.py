73a67dc937a28ee416ed29e1eac7b8c628339d87
bigy <121282845+bigy2012@users.noreply.github.com>
2023-10-30 16:42:30 +0700

    update all

--- main.py
+++ main.py
@@ -2,41 +2,79 @@ import pyautogui as pg
 import webbrowser
 import time
 import json
+import sys
 import datetime
+
 
 url = open('url.json')
 data = json.load(url)
 
+
 def autoClick(keyword, qty):
-    chat = pg.locateCenterOnScreen('3.png')
+        chat = pg.locateCenterOnScreen('3.png')
+        start, end = keyword.split("-")
 
-    if chat is not None:
-        pg.click(chat)
-        for i in range(1, qty + 1):
-            pg.typewrite(keyword)
-            pg.press('enter')
-    else:
-        print('ขออภัยผมหาห้องแชทไม่พบ!!')
-        time.sleep(5)
-        return False
+        if chat is not None:
+            pg.click(chat)
+            for i in range(int(start), int(end) + 1):
+                pg.typewrite(str(i))
+                pg.press('enter')
+        else:
+            print('ขออภัยผมหาห้องแชทไม่พบ!!')
+            time.sleep(5)
+            return False
+        main()
 
-# Open the web page
-webbrowser.open(data['url_01'])
-# Wait for the web page to load
-time.sleep(5)
+def convertTime(time, seconds):
+    hours, minutes = time.split(':')
+    second = seconds
+    down_second = int(seconds) - 0.2
+    down_minutes = int(minutes) - 1
 
-date_input = input("เวลาที่คุณต้องการให้เปิดใช้งาน (เช่น 13:30) : ")
-keyword = input('ข้อความของคุณ ? ')
-qty = int(data['qty'])
+    return
 
-while True:
+
+
+def process(keyword, qty, date_object, date_input) :
+    time_input_object = datetime.datetime.strptime(date_input, "%H:%M")
     current_time = datetime.datetime.now().strftime("%H:%M")
+    start = convertTime(current_time, datetime.datetime.now().strftime("%S"))
 
-    if current_time == date_input:
-        autoClick(keyword, qty)
-        break
+    while current_time != date_input:
+        print(datetime.datetime.now().strftime("%H:%M:%S"))
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        if time_input_object != date_input:
+            process(keyword, qty, date_object, date_input)
+        else:
+            autoClick(keyword, qty)
+            break
+    
+    autoClick(keyword, qty)
+    sys.exit()
+
+
+def main():
+    exTime = datetime.datetime.now().strftime("%H:%M")
+    date_input = input("เวลาที่คุณต้องการให้เปิดใช้งาน (เช่น "+ str(exTime) +") : ")
+    format_string = "%H:%M"
+    try:
+        date_object = datetime.datetime.strptime(date_input, format_string)
+    except ValueError:
+        print('เวลาของคุณไม่ถูกต้อง')
+        time.sleep(10)
+        return False
     else:
-        # Calculate the time to the next minute and sleep until then
-        time_to_next_minute = 60 - int(datetime.datetime.now().strftime("%S"))
-        time.sleep(time_to_next_minute)
-        print('โปรดรอให้ถึง ' + date_input)
+        keyword = input('ข้อความของคุณ ? ')
+        qty = int(data['qty'])
+
+    process(keyword, qty, date_object, date_input)
+    
+
+if __name__ == "__main__":
+    # webbrowser.open(data['url_01'])
+    main()
+
+
+
+
